0ea36c2b94d617a8cc5bf998aea79f8545a229ad
radiney <99511595+radiney@users.noreply.github.com>
2022-06-06 17:47:52 -0700

    Initial Commit

---
+++ videoStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  videoStreaming
+//
+//  Created by user217360 on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
